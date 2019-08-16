c38564839e2da265541375bb37315428ca69a316
oliverochman <oliver.ochman@hotmail.com>
2019-08-16 15:45:11 +0200

    basic LoginForm added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import CreateArticleForm from "./components/CreateArticleForm";
+import LoginForm from './components/LoginForm';
+
 
 function App() {
   return (
     <div>
       <h1>GNOSIS</h1>
+      <LoginForm />
+
       <CreateArticleForm />
     </div>
   );
