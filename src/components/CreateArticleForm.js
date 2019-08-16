12a182dd83c70aef63d2745622b37aa4fed5cc62
oliverochman <oliver.ochman@hotmail.com>
2019-08-16 14:51:30 +0200

    fixed button to render create article form

--- src/components/CreateArticleForm.js
+++ src/components/CreateArticleForm.js
@@ -7,18 +7,28 @@ class CreateArticleForm extends Component {
       title: "",
       body: "",
       errorMessage: "",
-      articleSaved: false
+      articleSaved: false,
+      renderArticleForm: false
     };
+
+  formHandler = () => {
+    this.setState({
+      renderArticleForm: !this.state.renderArticleForm,
+      articleSaved: false,
+      errorMessage: ''
+    })
+  }
 
   async onSave(e) {
     e.preventDefault();
     let response = await saveArticle(this.state.author, this.state.title, this.state.body);
     if (response.status === 200) {
       this.setState({
-        articleSaved: true
+        articleSaved: true,
+        renderArticleForm: false,
+        errorMessage: ''
       });
     } else {
-      debugger;
       this.setState({
         errorMessage: response.data.body.message
       })
@@ -30,47 +40,57 @@ class CreateArticleForm extends Component {
 
   render() {
     let articleStatus;
+    let form;
     if (this.state.articleSaved === true) {
       articleStatus = "Post successfully created";
     } else if (this.state.articleSaved === false && this.state.errorMessage !== '') {
-      debugger;
-
       articleStatus = this.state.errorMessage;
     }
 
+    if (this.state.renderArticleForm === true) {
+      form = (
+        <form id="post-article-form" onSubmit={e => this.onSave(e)}>
+          <div>
+            <label>Author</label>
+            <input
+              id="author"
+              value={this.state.author}
+              onChange={e => this.setState({ author: e.target.value })}
+            />
+          </div>
+          <div>
+            <label>Title</label>
+            <input
+              id="title"
+              value={this.state.title}
+              onChange={e => this.setState({ title: e.target.value })}
+            />
+          </div>
+          <div>
+            <label>Text</label>
+            <input
+              id="body"
+              value={this.state.body}
+              onChange={e => this.setState({ body: e.target.value })}
+            />
+          </div>
+          <input
+            id="submit-button"
+            type="submit"
+            value="Create"
+          />
+        </form>
+      )
+    }
+
     return (
-      <form id="post-article-form" onSubmit={e => this.onSave(e)}>
-        <div>
-          <label>Author</label>
-          <input
-            id="author"
-            value={this.state.author}
-            onChange={e => this.setState({ author: e.target.value })}
-          />
-        </div>
-        <div>
-          <label>Title</label>
-          <input
-            id="title"
-            value={this.state.title}
-            onChange={e => this.setState({ title: e.target.value })}
-          />
-        </div>
-        <div>
-          <label>Text</label>
-          <input
-            id="body"
-            value={this.state.body}
-            onChange={e => this.setState({ body: e.target.value })}
-          />
-        </div>
-        <input
-          id="submit-button"
-          type="submit"
-          value="Create"
-        />
-        <div>{articleStatus}</div>
-      </form>
+      <div id="create-article-component">
+        <button id="create-article" onClick={this.formHandler}>Create Article</button>
+
+        {form}
+        {articleStatus}
+      </div>
+      
     );
   }
 }
